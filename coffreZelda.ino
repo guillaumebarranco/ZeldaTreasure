1d5a250864f7e8123dade0f51821dde11eda4940
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2015-11-10 13:22:19 +0100

    Add comments to the script and rearrange the tabs and other few things code

--- coffreZelda.ino
+++ coffreZelda.ino
@@ -1,97 +1,112 @@
+// First we define the pins of the buzzer which will emit the sounds and the light sensor which will detect light changements
 #define BUZZER_PIN 6
 #define LDR 0
+
+/*
+* Then we set our variables to prepare the script 
+*/
+
+// Var relative to the light sensor
 int LDRValue = 0;
 int light_sensitivity = 50;
 
+// Var relative to the music
 int length = 4; // the number of notes
 char notes[] = "abcd";
-int beats[] = {1, 1, 1, 1};
 int tempo = 190;
 
-int a;
-int koko = 1;
+// Var relative to stop/restart and block the script
+int can_begin = 1;
 int lighted = 0;
 
+/*
+*   FUNCTIONS
+*/
+
 void setup() {
-  pinMode(BUZZER_PIN, OUTPUT);   
+    pinMode(BUZZER_PIN, OUTPUT);   
 }
 
 void loop() {
 
-  LDRValue = analogRead(LDR);
-  delay(200);
+    // We read the value of the light sensor
+    LDRValue = analogRead(LDR);
+    delay(200);
 
-  //*
-  if (LDRValue > light_sensitivity) {
+    // If the light value is > at 50 (which is the sensor in the dark maximum)
+    if (LDRValue > light_sensitivity) {
 
-    if(lighted == 0) {
-      koko = 0;
-      lighted = 1;
+        // We set that the script song can be executed
+        if(lighted == 0) {
+            can_begin = 0;
+            lighted = 1;
+        }
     }
-  }
 
-  if(koko == 0) {
+    // If the song can be executed
+    if(can_begin == 0) {
 
-    for (int z = 0; z < 8; ++z) {
+        for (int z = 0; z < 8; ++z) {
 
-      for(int i = 0; i < length; i++) {
+            for(int i = 0; i < length; i++) {
 
-        if(notes[i] == ' ') {
-          delay(beats[i] * tempo);
-        } else {
-          playNote(notes[i], beats[i] * tempo);
+                if(notes[i] == ' ') {
+                    delay(tempo);
+                } else {
+                    playNote(notes[i], tempo);
+                }
+
+                delay(tempo / 2);
+            }
+
+            tempo = tempo - 18;
+            can_begin = 1;
         }
 
-        delay(tempo / 2);
-      }
+        delay(1000);
+        tempo = 170;
 
-      tempo = tempo - 18;
-      koko = 1;
-    }
+        for(int i = 0; i < length; i++) {
 
-    delay(1000);
-    tempo = 170;
+            if(notes[i] == ' ') {
+                delay(tempo);
+            } else {
 
-    for(int i = 0; i < length; i++) {
+                if(i == 3) {
+                    playNote(notes[i], tempo * 5);
+                } else {
+                    playNote(notes[i], tempo);
+                }
+            }
 
-      if(notes[i] == ' ') {
-        delay(beats[i] * tempo);
-      } else {
-
-        if(i == 3) {
-          playNote(notes[i], tempo * 5);
-        } else {
-          playNote(notes[i], beats[i] * tempo);
+            delay(tempo / 2);
         }
-      }
 
-      delay(tempo / 2);
+        // We wait at least 5 seconds to rexecute the script if still enlighted
+        delay(5000);
+        lighted = 0;
     }
-
-    delay(5000);
-    lighted = 0;
-  }
-  //*/
 }
 
-/* play tone */
+// Function that plays the tone => one sound
 void playTone(int tone, int duration) {
-  for (long i = 0; i < duration * 1000L; i += tone * 2) {
-    digitalWrite(BUZZER_PIN, HIGH);
-    delayMicroseconds(tone);
-    digitalWrite(BUZZER_PIN, LOW);
-    delayMicroseconds(tone);
-  }
+    for (long i = 0; i < duration * 1000L; i += tone * 2) {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delayMicroseconds(tone);
+        digitalWrite(BUZZER_PIN, LOW);
+        delayMicroseconds(tone);
+    }
 }
 
+// Fonction that plays a "partition", few notes (4 actually)
 void playNote(char note, int duration) {
-  char names[] = {'a', 'b', 'c', 'd'};
-  int tones[] = {2200, 2100, 1950, 1900};
-  
-  // play the tone corresponding to the note name
-  for (int i = 0; i < 4; i++) {
-    if (names[i] == note) {
-      playTone(tones[i], duration);
+    char names[] = {'a', 'b', 'c', 'd'};
+    int tones[] = {2200, 2100, 1950, 1900};
+
+    // play the tone corresponding to the note name
+    for (int i = 0; i < 4; i++) {
+        if (names[i] == note) {
+            playTone(tones[i], duration);
+        }
     }
-  }
 }
